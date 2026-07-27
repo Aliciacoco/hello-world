085db661b21d184b2a6d11cb071396d602fae9ec
Aliciacoco <aliciakccoco@gmail.com>
2026-07-27 18:04:19 +0800

    feat: 删除导航栏积分tab，入口统一到右上角积分胶囊

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ export default function Navbar() {
           <ul className={styles.links}>
             <li><NavLink to="/" end className={({ isActive }) => isActive ? styles.active : ''}>练习</NavLink></li>
             <li><NavLink to="/bank" className={({ isActive }) => isActive ? styles.active : ''}>题库</NavLink></li>
-            <li><NavLink to="/points" className={({ isActive }) => isActive ? styles.active : ''}>积分</NavLink></li>
           </ul>
         </div>
         <NavPoints />
